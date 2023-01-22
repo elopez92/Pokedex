5d4dd3750b47b40999cf7917d5ef86225517c656
elopez <elopez2192@gmail.com>
2023-01-21 18:59:37 -0800

    Setup retrofit

--- app/src/main/java/com/elopez/pokedex/di/AppModule.kt
+++ app/src/main/java/com/elopez/pokedex/di/AppModule.kt
@@ -1,10 +1,34 @@
 package com.elopez.pokedex.di
 
+import com.elopez.pokedex.data.remote.PokeApi
+import com.elopez.pokedex.repository.PokemonRepository
+import com.elopez.pokedex.util.Constants.BASE_URL
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.create
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object AppModule {
+
+    @Singleton
+    @Provides
+    fun providePokemonRepository(
+        api: PokeApi
+    ) = PokemonRepository(api)
+
+    @Singleton
+    @Provides
+    fun providePokeApi() : PokeApi{
+        return Retrofit.Builder()
+            .addConverterFactory(GsonConverterFactory.create())
+            .baseUrl(BASE_URL)
+            .build()
+            .create(PokeApi::class.java)
+    }
 }
